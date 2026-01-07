471a540fd7708bee83dc14013f10fa9d9a261751
Patricia Hawley <hawleypa@ieee.org>
2026-01-07 18:57:34 -0500

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -3,10 +3,10 @@ const NodeHelper = require("node_helper")
 module.exports = NodeHelper.create({
 
   async socketNotificationReceived(notification, payload) {
-    if (notification === "GET_RANDOM_TEXT") {
+    if (notification === "GET_QUOTE") {
       const amountCharacters = payload.amountCharacters || 10
       const randomText = Array.from({ length: amountCharacters }, () => String.fromCharCode(Math.floor(Math.random() * 26) + 97)).join("")
-      this.sendSocketNotification("EXAMPLE_NOTIFICATION", { text: randomText })
+      this.sendSocketNotification("RECEIVED_QUOTE", { text: randomText })
     }
   },
 })
